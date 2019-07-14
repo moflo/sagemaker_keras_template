26274b0659c80276a1b9f0fd01275c6bd043f844
moflo <github@moflo.me>
2019-07-13 18:12:22 -0700

    Update cifar10_keras_sage_train.py

--- cifar10_keras_sage_train.py
+++ cifar10_keras_sage_train.py
@@ -1,9 +1,9 @@
-'''
+"""
 # CIFAR10 SageMaker Keras Training Function.
 
 Use the SageMaker SDK Script syntax to train a Keras CNN.
 
-'''
+"""
 
 from __future__ import print_function
 import argparse
@@ -18,86 +18,117 @@ from cifar10_keras_sage_model import get_model
 
 
 def parse_args():
-    
+
     parser = argparse.ArgumentParser()
 
     # hyperparameters sent by the client are passed as command-line arguments to the script
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=64)
-    
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument(
+        "--batch_size", type=int, default=64
+    )
+
     # data directories
-    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
-    parser.add_argument('--val', type=str, default=os.environ.get('SM_CHANNEL_VAL'))
-        
+    parser.add_argument(
+        "--train",
+        type=str,
+        default=os.environ.get("SM_CHANNEL_TRAIN"),
+    )
+    parser.add_argument(
+        "--val",
+        type=str,
+        default=os.environ.get("SM_CHANNEL_VAL"),
+    )
+
     # model directory: we will use the default set by SageMaker, /opt/ml/model
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
-    
+    parser.add_argument(
+        "--model_dir",
+        type=str,
+        default=os.environ.get("SM_MODEL_DIR"),
+    )
+
     return parser.parse_known_args()
 
 
 def get_train_data(train_dir):
-    
-    x_train = np.load(os.path.join(train_dir, 'x_train.npy'))
-    y_train = np.load(os.path.join(train_dir, 'y_train.npy'))
-    print('x train', x_train.shape,'y train', y_train.shape)
+
+    x_train = np.load(
+        os.path.join(train_dir, "x_train.npy")
+    )
+    y_train = np.load(
+        os.path.join(train_dir, "y_train.npy")
+    )
+    print(
+        "x train", x_train.shape, "y train", y_train.shape
+    )
 
     return x_train, y_train
 
 
 def get_val_data(val_dir):
-    
-    x_val = np.load(os.path.join(val_dir, 'x_val.npy'))
-    y_val = np.load(os.path.join(val_dir, 'y_val.npy'))
-    print('x val', x_val.shape,'y val', y_val.shape)
+
+    x_val = np.load(os.path.join(val_dir, "x_val.npy"))
+    y_val = np.load(os.path.join(val_dir, "y_val.npy"))
+    print("x val", x_val.shape, "y val", y_val.shape)
 
     return x_val, y_val
- 
+
+
 def main(args):
     x_train, y_train = get_train_data(args.train)
     x_val, y_val = get_val_data(args.val)
-    
+
     # Convert class vectors to binary class matrices.
     num_classes = 10
 
-    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_train = keras.utils.to_categorical(
+        y_train, num_classes
+    )
     y_val = keras.utils.to_categorical(y_val, num_classes)
 
-    model = get_model(x_train.shape[1:],num_classes)
+    model = get_model(x_train.shape[1:], num_classes)
 
     # initialize global variables in session (required during save)
     with tf.Session() as sess:
-      sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer())
 
     # initiate RMSprop optimizer
     opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 
     # Let's train the model using RMSprop
-    model.compile(loss='categorical_crossentropy',
-                optimizer=opt,
-                metrics=['accuracy'])
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=opt,
+        metrics=["accuracy"],
+    )
 
-    x_train = x_train.astype('float32')
-    x_val = x_val.astype('float32')
+    x_train = x_train.astype("float32")
+    x_val = x_val.astype("float32")
     x_train /= 255
     x_val /= 255
 
-    model.fit(x_train, y_train,
-              batch_size=args.batch_size,
-              epochs=args.epochs,
-            #   callbacks=[time_callback],
-              validation_data=(x_val, y_val),
-              shuffle=True)
-    
+    model.fit(
+        x_train,
+        y_train,
+        batch_size=args.batch_size,
+        epochs=args.epochs,
+        #   callbacks=[time_callback],
+        validation_data=(x_val, y_val),
+        shuffle=True,
+    )
 
     # create a TensorFlow SavedModel for deployment to a SageMaker endpoint with TensorFlow Serving
-    model_version_dir = os.path.join(args.model_dir, f'model-{time.time() * 10}')
+    model_version_dir = os.path.join(
+        args.model_dir, f"model-{time.time() * 10}"
+    )
     os.makedirs(model_version_dir, exist_ok=True)
 
     # Save the h5 model
-    model_h5 = os.path.join(model_version_dir, 'model.h5')
+    model_h5 = os.path.join(model_version_dir, "model.h5")
     model.save(model_h5)
 
-    saved_model = os.path.join(model_version_dir, 'saved_model')
+    saved_model = os.path.join(
+        model_version_dir, "saved_model"
+    )
     os.makedirs(saved_model, exist_ok=True)
 
     # Error : model.save_weights(checkpoint_prefix, save_format='tf', overwrite=True) TypeError: save_weights() got an unexpected keyword argument 'save_format'
@@ -116,11 +147,7 @@ def main(args):
     # tf.keras.experimental.export_saved_model(model, saved_model)
 
 
-
-
 if __name__ == "__main__":
-            
+
     args, _ = parse_args()
     main(args)
-
-    
